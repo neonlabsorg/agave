0a9e15a99b53b353d375d42cf421dd1fba820530
Vladimir Bugaev <boogvar@gmail.com>
2025-11-28 13:38:20 +0700

    MAX_PERMITTED_DATA_INCREASE

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -5363,14 +5363,14 @@ impl Bank {
         if new_feature_activations.contains(&feature_set::pico_inflation::id()) {
             *self.inflation.write().unwrap() = Inflation::pico();
             self.fee_rate_governor.burn_percent = solana_fee_calculator::DEFAULT_BURN_PERCENT; // 50% fee burn
-            self.rent_collector.rent.burn_percent = 50; // 50% rent burn
+            self.rent_collector.rent.burn_percent = 0; // 50% rent burn
         }
 
         if !new_feature_activations.is_disjoint(&self.feature_set.full_inflation_features_enabled())
         {
             *self.inflation.write().unwrap() = Inflation::full();
             self.fee_rate_governor.burn_percent = solana_fee_calculator::DEFAULT_BURN_PERCENT; // 50% fee burn
-            self.rent_collector.rent.burn_percent = 50; // 50% rent burn
+            self.rent_collector.rent.burn_percent = 0; // 50% rent burn
         }
 
         self.apply_new_builtin_program_feature_transitions(&new_feature_activations);
