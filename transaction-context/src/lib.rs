2c25d31cdcfcbcc1bf937d03125e7a721221110b
Vladimir Bugaev <boogvar@gmail.com>
2026-02-16 20:34:31 +0700

    fix max account size

--- transaction-context/src/lib.rs
+++ transaction-context/src/lib.rs
@@ -25,12 +25,12 @@ pub const MAX_ACCOUNTS_PER_TRANSACTION: usize = 256;
 // one index is used as NON_DUP_MARKER in ABI v0 and v1.
 pub const MAX_ACCOUNTS_PER_INSTRUCTION: usize = 255;
 pub const MAX_INSTRUCTION_DATA_LEN: usize = 10 * 1024;
-pub const MAX_ACCOUNT_DATA_LEN: u64 = 10 * 1024 * 1024;
+pub const MAX_ACCOUNT_DATA_LEN: u64 = 32 * 1024 * 1024;
 // Note: With stricter_abi_and_runtime_constraints programs can grow accounts
 // faster than they intend to, because the AccessViolationHandler might grow
 // an account up to MAX_ACCOUNT_DATA_GROWTH_PER_INSTRUCTION at once.
 pub const MAX_ACCOUNT_DATA_GROWTH_PER_TRANSACTION: i64 = MAX_ACCOUNT_DATA_LEN as i64 * 2;
-pub const MAX_ACCOUNT_DATA_GROWTH_PER_INSTRUCTION: usize = 10 * 1_024;
+pub const MAX_ACCOUNT_DATA_GROWTH_PER_INSTRUCTION: usize = 10 * 10 * 1_024;
 
 #[cfg(test)]
 static_assertions::const_assert_eq!(
