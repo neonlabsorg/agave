ac64237721f360fda2314c4ef78b9138d5529b2e
Kristina Nikolaeva <kn@neonlabs.org>
2026-05-05 15:01:09 +0200

    CI for parasol fork dev

---
+++ ci/parasol-node-run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+solana-test-validator \
+  --ticks-per-slot 16 \
+  --ledger /opt/parasol/data \
+  --rpc-tx-type-map-config /usr/local/bin/tx_type_rules.yml \
+  --rpc-port 8899 \
+  --faucet-port 19900 \
+  --gossip-port 18000 \
+  --limit-ledger-size 1000000 \
+  --log
